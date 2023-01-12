211de4cd1906d8c5086090c6a6e32aaf564f8a85
ValdemarSheesh <demon99vovan@gmail.com>
2023-01-12 18:57:44 +0300

    added update

--- src/main/java/com/example/Bank/controller/ClientController.java
+++ src/main/java/com/example/Bank/controller/ClientController.java
@@ -6,8 +6,6 @@ import com.example.Bank.mapper.ClientMapper;
 import com.example.Bank.model.Client;
 import com.example.Bank.service.impl.ClientServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -23,18 +21,24 @@ public class ClientController {
     private ClientServiceImpl clientService;
 
     @PostMapping("save")
-    public ResponseEntity<?> saveClient(@Validated @RequestBody ClientDto clientDto, BindingResult bindingResult) {
+    public void saveClient(@Validated @RequestBody ClientDto clientDto, BindingResult bindingResult) {
         if (bindingResult.hasErrors())
             throw new InvalidValueException("Invalid value");
         Client client = ClientMapper.INSTANCE.clientDtoToClient(clientDto);
         clientService.addClient(client);
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ClientDto getClient(@PathVariable Long id) {
         Client client = clientService.getClientById(id);
         return ClientMapper.INSTANCE.clientToClientDto(client);
+    }
+
+    @PutMapping("/update")
+    public void updateClient(@RequestParam(value = "id") Long id, @Validated @RequestBody ClientDto clientDto) {
+        Client client = ClientMapper.INSTANCE.clientDtoToClient(clientDto);
+        client.setId(id);
+        clientService.editClient(client);
     }
 
     @GetMapping()
