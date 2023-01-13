ad89d9e18eb8a4349f8bb9aae66c1e4ad6f52fe8
ValdemarSheesh <demon99vovan@gmail.com>
2023-01-13 14:16:09 +0300

    refactoring

--- src/main/java/com/example/Bank/repo/BankRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Bank.repo;
-
-import com.example.Bank.model.Bank;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-public interface BankRepository extends JpaRepository<Bank, Long> {
-}
