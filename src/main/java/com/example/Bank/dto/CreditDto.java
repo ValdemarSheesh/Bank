8453fcc2eac8ec0872db1c7fc5f762b967473aad
ValdemarSheesh <demon99vovan@gmail.com>
2023-01-13 16:42:16 +0300

    added dto

---
+++ src/main/java/com/example/Bank/dto/CreditDto.java
@@ -0,0 +1,24 @@
+package com.example.Bank.dto;
+
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CreditDto {
+
+    @Digits(integer = Integer.MAX_VALUE, fraction = 2)
+    @Positive
+    private BigDecimal creditLimit;
+    @Positive
+    @Digits(integer = 3, fraction = 2)
+    private double interestRate;
+}
---
+++ src/main/java/com/example/Bank/dto/PaymentScheduleDto.java
@@ -0,0 +1,30 @@
+package com.example.Bank.dto;
+
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PaymentScheduleDto {
+    @Pattern(regexp = "dd.MM.yyy")
+    private LocalDate paymentDate;
+    @Digits(integer = Integer.MAX_VALUE, fraction = 2)
+    @Positive
+    private BigDecimal paymentAmount;
+    @Digits(integer = Integer.MAX_VALUE, fraction = 2)
+    @Positive
+    private BigDecimal repaymentAmountOfCreditBody;
+    @Digits(integer = Integer.MAX_VALUE, fraction = 2)
+    @Positive
+    private double repaymentAmountPercent;
+}
