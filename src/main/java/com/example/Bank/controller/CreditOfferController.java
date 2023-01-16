7c64244faa08cbe3b052d0d10097c71bd5861281
ValdemarSheesh <demon99vovan@gmail.com>
2023-01-16 15:40:00 +0300

    added update, allCreditOffers, delete

---
+++ src/main/java/com/example/Bank/controller/CreditOfferController.java
@@ -0,0 +1,95 @@
+package com.example.Bank.controller;
+
+import com.example.Bank.dto.CreditOfferDto;
+import com.example.Bank.exceptions.InvalidValueException;
+import com.example.Bank.mapper.impl.CreditOfferMapperImpl;
+import com.example.Bank.model.Client;
+import com.example.Bank.model.CreditOffer;
+import com.example.Bank.service.impl.ClientServiceImpl;
+import com.example.Bank.service.impl.CreditOfferServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Validated
+@RestController
+@RequestMapping("credit-offers")
+public class CreditOfferController {
+
+    @Autowired
+    CreditOfferServiceImpl creditOfferService;
+    @Autowired
+    ClientServiceImpl clientService;
+    @Autowired
+    CreditOfferMapperImpl creditOfferMapper;
+
+    @PostMapping
+    public ResponseEntity<String> saveCreditOffer(@Validated @RequestBody CreditOfferDto creditOfferDto,
+                                                  BindingResult bindingResult,
+                                                  @RequestParam(value = "clientId") Long clientId) {
+        if (bindingResult.hasErrors())
+            throw new InvalidValueException(InvalidValueException.createMessage(bindingResult));
+
+        CreditOffer creditOffer = creditOfferMapper.creditOfferDtoToCreditOffer(creditOfferDto);
+        Client client = clientService.getClientById(clientId);
+
+        creditOffer.setClient(client);
+        creditOfferService.addCreditOffer(creditOffer);
+
+        return ResponseEntity.ok("Credit offer added");
+    }
+
+    @GetMapping("/{id}")
+    public CreditOfferDto getCreditOffer(@PathVariable Long id) {
+        CreditOffer creditOffer = creditOfferService.getCreditOfferById(id);
+        return creditOfferMapper.creditOfferToCreditOfferDto(creditOffer);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateCreditOffer(@RequestParam(value = "creditOfferId") Long creditOfferId,
+                                                    @Validated @RequestBody CreditOfferDto creditOfferDto,
+                                                    BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            throw new InvalidValueException(InvalidValueException.createMessage(bindingResult));
+
+        CreditOffer creditOffer = creditOfferMapper.creditOfferDtoToCreditOffer(creditOfferDto);
+        Client client = clientService.getClientById(creditOfferId);
+
+        creditOffer.setId(creditOfferId);
+        creditOffer.setClient(client);
+        creditOfferService.editCreditOffer(creditOffer);
+
+        return ResponseEntity.ok("Credit offers updated");
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllCreditOffersByClient(@RequestParam(value = "clientId") Long clientId) {
+        Client client = clientService.getClientById(clientId);
+        List<CreditOffer> creditOffers = creditOfferService.getAllCreditOfferByClient(client);
+        if (creditOffers.isEmpty())
+            return ResponseEntity.ok("Credit offers is empty");
+        else
+            return ResponseEntity.ok(creditOfferMapper.creditOffersToCreditOffersDto(creditOffers));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteCreditOffer(@RequestParam(value = "creditOfferId") Long creditOfferId) {
+        creditOfferService.deleteCreditOffer(creditOfferId);
+        return ResponseEntity.ok("Credit offer deleted");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
