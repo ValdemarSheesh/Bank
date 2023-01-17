6ad1dd112b0c666b4e3fc2d390267285003a8539
ValdemarSheesh <demon99vovan@gmail.com>
2023-01-17 13:04:10 +0300

    delete

--- src/main/java/com/example/Bank/repo/CreditOfferRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Bank.repo;
-
-import com.example.Bank.model.CreditOffer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CreditOfferRepository extends JpaRepository<CreditOffer, Long> {
-}
--- src/main/java/com/example/Bank/repo/CreditRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Bank.repo;
-
-import com.example.Bank.model.Credit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CreditRepository extends JpaRepository<Credit, Long> {
-}
