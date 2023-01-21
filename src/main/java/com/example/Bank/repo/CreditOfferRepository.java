07d61639976f4e8662ba484aefc7e35e3fcd3ce7
ValdemarSheesh <demon99vovan@gmail.com>
2023-01-21 13:49:41 +0300

    refactoring

---
+++ src/main/java/com/example/Bank/repo/CreditOfferRepository.java
@@ -0,0 +1,7 @@
+package com.example.Bank.repo;
+
+import com.example.Bank.model.CreditOffer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CreditOfferRepository extends JpaRepository<CreditOffer, Long> {
+}
